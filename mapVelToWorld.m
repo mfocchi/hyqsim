f1521e5547f34c96eff9ccce142020dc7772ddb6
Michele Focchi <michele.focchi@iit.it>
2019-03-04 12:46:20 +0100

    [bugfix] in mapVepToWorld

--- mapVelToWorld.m
+++ mapVelToWorld.m
@@ -3,5 +3,5 @@ function output = mapVelToWorld(input)
 baseTwistB=evalin('base', 'baseTwistB');
 b_R_w = evalin('base', 'b_R_w');
 
-output = b_R_w'*baseTwistB(4:6) + b_R_w'*(cross(baseTwistB(4:6),input));
+output = b_R_w'*baseTwistB(4:6) + b_R_w'*(cross(baseTwistB(1:3),input));
 end
