8045cfb9ebfcebb220d8472452f230f7990b436f
Michele Focchi <michele.focchi@iit.it>
2019-03-08 18:25:51 +0100

    bugfix in computeJCb Jcq

--- computeJcq.m
+++ computeJcq.m
@@ -12,17 +12,17 @@ Jq = zeros(size(stance_legs,2),  12);
 count=1;
 for i=1:size(stance_legs,2)
     if strcmp(stance_legs{i}, 'LF')
-        Jq(count:count+2,1:3) = b_R_w*J.LF;
+        Jq(count:count+2,1:3) = b_R_w'*J.LF;
     end
     if strcmp(stance_legs{i}, 'RF')
-        Jq(count:count+2,4:6) = b_R_w*J.RF;
+        Jq(count:count+2,4:6) = b_R_w'*J.RF;
     end
     if strcmp(stance_legs{i}, 'LH')
-        Jq(count:count+2,7:9) = b_R_w*J.LH;
+        Jq(count:count+2,7:9) = b_R_w'*J.LH;
         
     end
     if strcmp(stance_legs{i}, 'RH')
-       Jq(count:count+2,10:12) = b_R_w*J.RH;
+       Jq(count:count+2,10:12) = b_R_w'*J.RH;
     end
     count = count +3;
 
