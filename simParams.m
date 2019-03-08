e812b52f51787e056aa784959d1a5bd422cf5e74
Michele Focchi <michele.focchi@iit.it>
2019-03-08 18:28:40 +0100

    tuned simulink such that trunk controller works in parallel with joint PD (still unstable)

--- simParams.m
+++ simParams.m
@@ -1,15 +1,16 @@
 
 %terrain info
 groundLevel = 0.0;
-stiffness = 40000; %cannot be higher otherwise the simulink simulation gets unstable
-damping = 100;    
+stiffness = 20000; %cannot be higher otherwise the simulink simulation gets unstable
+damping = 100;   
+THRESHOLD = -0.001; %ground penetration to trigger contact
 
 %joint controller info
-Kj = 500;
-Dj = 30;
+Kj = 300;
+Dj = 20;
 
 %solver
-simulationDuration = 0.15;
+simulationDuration = 1;
 dt = 0.001; %integration step
 
 %flags
--- simulink/mbdoParams.m
+++ simulink/mbdoParams.m
@@ -5,26 +5,28 @@
     clc
     
    addpath('../') 
+   addpath('../hyq')
+   addpath('../rotations')
    initVars 
    simParams   
    updateStates([q;qd;basePoseW; baseTwistB])
-   desBasePos = basePoseW;     
+   desBasePoseW = basePoseW;     
    plot_plane_through_point([0;0;1],[0;0;groundLevel],1);
    stance_legs = {'LF','RF','LH','RH'};
-   gravityWrenchB = [ zeros(3,1);b_R_w*[0;0;-9.81]];
+   gravityB = [ zeros(3,1);b_R_w*[0;0;-9.81]];
    grForcesB = zeros(12,1);
-    % Jq=computeJcq(stance_legs, J,b_R_w);
-    % Jb=computeJcb(stance_legs, feet,b_R_w);
      
    %trunk controller
-   Kplin = eye(3)*2000;
-   Kdlin = eye(3)*600;
-   Kpang = eye(3)*2000;
-   Kdang = eye(3)*200;
+   Kplin = eye(3)*1000;
+   Kdlin = eye(3)*200;
+   Kpang = eye(3)*1000;
+   Kdang = eye(3)*100;
    
    %observer gains
-   Gain(4:6,4:6) = eye(3)*100;% linear case
-   Gain(1:3,1:3) = eye(3)*100; %angular case 
+   Gain(4:6,4:6) = eye(3)*50;% linear case
+   Gain(1:3,1:3) = eye(3)*10; %angular case 
 
    %ext disturbance index
    inputForce =3;
+
+   resetDesBasePos = false;
